4522eb0f769d0a1d978ac1f9970f340d65b8896d
husseinayad <89656515+husseinayad@users.noreply.github.com>
2021-09-12 23:26:09 +0000

    Add files via upload

---
+++ atelier.js
@@ -0,0 +1,10 @@
+/*function additionne(nbr1, nbr2){
+	alert(Number(nbr1) + Number(nbr2));
+}
+var prompt1=prompt('votre nombre1');
+var prompt2=prompt('votre nombre2');
+
+additionne(prompt1, prompt2);
+
+
+ 
